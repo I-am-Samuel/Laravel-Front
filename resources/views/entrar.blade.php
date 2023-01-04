5f7d1dc3d002b0537700a062e2d09b0b82708538
I-am-Samuel <samueleduardorocha@gmail.com>
2023-01-03 23:26:48 -0300

    first commit

---
+++ resources/views/entrar.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Entrar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <div class="login">
+
+        <h1 class="text-center">Entrar</h1>
+        <br>
+
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div><br>
+        <div class="form-group form-check">
+            <input class="form-check-input" type="checkbox" id="check">
+            <label class="form-check-label" for="check">Remember me</label>
+        </div>
+        <br>
+        <button type="button" class="btn btn-primary btn-lg">Large button</button>
+
+    </div>
+
+    <div class= "logo">
+        <img src="/img/folderblue_92960 1.png" alt="logo">
+    </div>
+  </body>
+</html>
